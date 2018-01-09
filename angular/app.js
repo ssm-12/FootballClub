f130d549778d082d41fa3ec02cec609cbee5e800
Souvik Roy <souvikroy.it@gmail.com>
2018-01-10 01:48:08 +0530

    Major change $q.all included for getting multiple through http request

--- angular/app.js
+++ angular/app.js
@@ -1,23 +1,52 @@
 var myApp = angular.module('footballApp',['ngRoute']);
 
 //Main controller
-myApp.controller('allDataController',['$http',function($http){
+myApp.controller('allDataController',['$http','$q',function($http,$q){
   var main = this;
-  this.jsonURL = 'https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json';
-  console.log(this.jsonURL);
+
+  //Data Source URLs
+  this.jsonURL2015 = 'https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.json';
+  this.jsonURL2016 = 'https://raw.githubusercontent.com/openfootball/football.json/master/2016-17/en.1.json';
+
   //Load all match details
   this.loadAllMatchDetails = function(){
 
-      console.log('Load All Matches Called');
-      $http({
-        method: 'GET',
-        url: main.jsonURL
-      }).then(function successHandler(response) {
-          console.log(response);
-          main.allMatches = response.data;
-        }, function errorHandler(response){
-          console.log(response);
+      //Using $q.all to get multiple data through http request
+      main.match_list_1 = $http.get(main.jsonURL2015, {'cache': false});
+      main.match_list_2 = $http.get(main.jsonURL2015, {'cache': false});
 
-        })
+      $q.all([main.match_list_1, main.match_list_2]).then(function(values) {
+          console.log(values);
+      });
+      //$q.all - Ends Here
+
+
+
+      //Function to get data through http request
+      // var getData = function(linkURL){
+      //   $http({
+      //       method: 'GET',
+      //       url: linkURL
+      //   }).then(function successHandler(response) {
+      //       console.log('1');
+      //       console.log(response);
+      //       angular.forEach(response.data.rounds, function(round) {
+      //           main.round = round;
+      //           angular.forEach(round.matches, function(match) {
+      //             match.roundName = main.round.name;
+      //             main.allMatches.push(match);
+      //           });
+      //       });
+      //       console.log('2');
+      //       console.log(main.allMatches);
+      //     }, function errorHandler(response){
+      //       console.log(response);
+      //     })
+      // }
+      //
+      // main.allMatches = [];
+      // getData(main.jsonURL2015);
+      // console.log('3');
+      // console.log(main.allMatches);
   }
 }]);
